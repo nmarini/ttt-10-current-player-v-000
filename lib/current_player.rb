660bc2088f2b0ade912742aeaede288dffc6b740
Nicolas Marini <nmarini44@gmail.com>
2018-04-25 00:19:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,4 @@
-def turn_count(board)
-counter = 0
-  board.each do |position|
-    counter = 0
-      board.each do |position|
-        if position == "X" || position == "O"
-          counter += 1
-        end
-      end
-      counter
-    end
 
 def current_player(baord)
 
 end
-
-      
